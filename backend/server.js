e607165e7caea7f9c3cb78e9bf6ddfdb300fd061
danica <liwandan@gmail.com>
2021-03-24 09:52:13 +1300

    Fetched Trefle data

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const fetch = require("node-fetch");
+const express = require("express");
+const app = express();
+
+app.get("/", async (request, response) => {
+  const params = {
+    origin: "http://localhost:3000",
+    token: process.env.SECRET,
+  };
+
+  const tokenResponse = await fetch("https://trefle.io/api/auth/claim", {
+    method: "post",
+    body: JSON.stringify(params),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const json = await tokenResponse.json();
+  console.log(json);
+  response.writeHead(200, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Methods": "GET, PATCH, POST, PUT, DELETE, OPTIONS",
+  });
+  response.end(JSON.stringify(json));
+  console.log(Date.now() + " Ping Received");
+});
+app.listen(process.env.PORT);
