4f131775cade3ed5ef4c7f8f126b807cf01924cc
danica <liwandan@gmail.com>
2021-03-25 12:24:34 +1300

    Added icon hover by hook and event

--- src/components/search-page/PageButton.js
+++ src/components/search-page/PageButton.js
@@ -1,15 +1,15 @@
-import React, { Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import styled, { css } from "styled-components";
 import { Button } from "../../theme/globalStyle";
 import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 
 const StyledButton = styled(Button)`
+  position: fixed;
+  top: 50%;
   width: 4rem;
   height: 4rem;
   border-radius: 2rem;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  position: fixed;
-  top: 50%;
   cursor: pointer;
   ${(props) =>
     props.right &&
@@ -27,27 +27,35 @@ const StyledButton = styled(Button)`
   }
 `;
 
-const arrowIcon = {
-  fontSize: "4rem",
-  color: "#FF9000",
-};
-
 const PageButton = () => {
+  const [iconColorRight, setIconColorRight] = useState("#ffa83f");
+  const [iconColorLeft, setIconColorLeft] = useState("#ffa83f");
+
+  const arrowIconRight = {
+    fontSize: "4rem",
+    color: `${iconColorRight}`,
+  };
+
+  const arrowIconLeft = {
+    fontSize: "4rem",
+    color: `${iconColorLeft}`,
+  };
+
   return (
     <Fragment>
-      <StyledButton right>
-        <FaArrowCircleRight
-          style={arrowIcon}
-          onMouseOut={({ target }) => (target.style.color = "#ffaa42")}
-          onMouseOver={({ target }) => (target.style.color = "#FF9000")}
-        />
+      <StyledButton
+        right
+        onMouseEnter={() => setIconColorRight("#FF9000")}
+        onMouseLeave={() => setIconColorRight("#ffa83f")}
+      >
+        <FaArrowCircleRight style={arrowIconRight} />
       </StyledButton>
-      <StyledButton left>
-        <FaArrowCircleLeft
-          style={arrowIcon}
-          onMouseOut={({ target }) => (target.style.color = "#ffaa42")}
-          onMouseOver={({ target }) => (target.style.color = "#FF9000")}
-        />
+      <StyledButton
+        left
+        onMouseEnter={() => setIconColorLeft("#FF9000")}
+        onMouseLeave={() => setIconColorLeft("#ffa83f")}
+      >
+        <FaArrowCircleLeft style={arrowIconLeft} />
       </StyledButton>
     </Fragment>
   );
