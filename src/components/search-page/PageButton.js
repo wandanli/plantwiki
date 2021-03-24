7b0e377229ef6539459269a105f6d1e890961007
danica <liwandan@gmail.com>
2021-03-25 11:57:05 +1300

    Added icon hover by inline styled

--- src/components/search-page/PageButton.js
+++ src/components/search-page/PageButton.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from "react";
 import styled, { css } from "styled-components";
 import { Button } from "../../theme/globalStyle";
-import { IconContext } from "react-icons";
 import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 
 const StyledButton = styled(Button)`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 2rem;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   position: fixed;
   top: 50%;
   cursor: pointer;
@@ -24,39 +27,27 @@ const StyledButton = styled(Button)`
   }
 `;
 
-const StyledIcon = styled.div`
-  width: 4rem;
-  height: 4rem;
-  border-radius: 2rem;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-`;
+const arrowIcon = {
+  fontSize: "4rem",
+  color: "#FF9000",
+};
 
 const PageButton = () => {
   return (
     <Fragment>
       <StyledButton right>
-        <IconContext.Provider
-          value={{
-            color: "#FF9000", //how to use theme.color here?
-            size: "4rem",
-          }}
-        >
-          <StyledIcon>
-            <FaArrowCircleRight />
-          </StyledIcon>
-        </IconContext.Provider>
+        <FaArrowCircleRight
+          style={arrowIcon}
+          onMouseOut={({ target }) => (target.style.color = "#ffaa42")}
+          onMouseOver={({ target }) => (target.style.color = "#FF9000")}
+        />
       </StyledButton>
       <StyledButton left>
-        <IconContext.Provider
-          value={{
-            color: "#FF9000", //how to use theme.color here?
-            size: "4rem",
-          }}
-        >
-          <StyledIcon>
-            <FaArrowCircleLeft />
-          </StyledIcon>
-        </IconContext.Provider>
+        <FaArrowCircleLeft
+          style={arrowIcon}
+          onMouseOut={({ target }) => (target.style.color = "#ffaa42")}
+          onMouseOver={({ target }) => (target.style.color = "#FF9000")}
+        />
       </StyledButton>
     </Fragment>
   );
