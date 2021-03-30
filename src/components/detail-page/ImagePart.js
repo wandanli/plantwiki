39dbedb97982da90b29a15a5fd4dce123d0ebe27
danica <liwandan@gmail.com>
2021-03-31 11:02:51 +1300

    Added onclick display slected images

--- src/components/detail-page/ImagePart.js
+++ src/components/detail-page/ImagePart.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect, Fragment } from "react";
 import styled from "styled-components";
-import {
-  Heading,
-  Image,
-  Paragraph,
-  Wrapper,
-  Button,
-} from "../../theme/globalStyle";
+import { Heading, Image, Wrapper, Button } from "../../theme/globalStyle";
 
 const StyledImage = styled(Image)`
   margin: 20px;
@@ -18,19 +12,34 @@ const StyledWrapper = styled(Wrapper)`
   /* display: ${(props) => (props.className === "" ? "block" : "none")}; */
 `;
 
+const StyledButton = styled(Button)`
+  margin: 10px;
+  padding: 10px;
+  width: 100px;
+  height: 40px;
+  background-color: ${(props) => props.theme.color.primary};
+  border-radius: 30px;
+  color: ${(props) => props.theme.color.white};
+  font-size: ${(props) => props.theme.fontSize.large};
+`;
+
 const ImagePart = ({ images }) => {
   const [imagesKey, setImagesKey] = useState([]);
+  const [toggles, setToggles] = useState({});
 
-  const getImagesKey = () => {
-    const imagesKeyArray = [];
+  const handleImagesKey = () => {
+    let imagesKeyArray = [];
+    let imagesKeyToggle = new Map();
     for (let key in images) {
       imagesKeyArray.push(key);
+      imagesKeyToggle.set(key, false);
     }
     setImagesKey(imagesKeyArray);
+    setToggles(imagesKeyToggle);
   };
 
   useEffect(() => {
-    getImagesKey();
+    handleImagesKey();
   }, [images]);
 
   const DisplayImages = ({ value }) => {
@@ -50,6 +59,18 @@ const ImagePart = ({ images }) => {
     );
   };
 
+  const handleDisplayClick = (key) => {
+    toggles.forEach((value, toogleKey) => {
+      if (toogleKey === key) {
+        toggles.set(toogleKey, !value);
+      } else {
+        toggles.set(toogleKey, false);
+      }
+    });
+    const newToggles = new Map(toggles);
+    setToggles(newToggles);
+  };
+
   return (
     <Wrapper>
       <Wrapper flexDirection="column">
@@ -57,20 +78,32 @@ const ImagePart = ({ images }) => {
           Images
         </Heading>
         <Wrapper>
-          {/* {Object.entries(images).map(([key, value]) => {
-            if (key === "") {
-              return <DisplayImages value={value} />;
-            }
-            return null;
-          })} */}
           {Object.entries(images).map(([key, value]) => {
             return key === "" ? <DisplayImages value={value} /> : null;
           })}
         </Wrapper>
         <Wrapper>
-          {imagesKey.map((key, index) => (
-            <Button key={index}>{key}</Button>
-          ))}
+          {imagesKey.map((key, index) =>
+            key === "" ? null : (
+              <StyledButton
+                onClick={(e) => handleDisplayClick(key)}
+                key={index}
+              >
+                {key}
+              </StyledButton>
+            )
+          )}
+        </Wrapper>
+        <Wrapper>
+          {Object.entries(images).map(([key, value]) => {
+            const ret =
+              toggles.get(key) === true ? (
+                <DisplayImages value={value} />
+              ) : null;
+            console.log(`=== ${key}: ${toggles.get(key)}, ret ${ret}`);
+            return ret;
+            // return <DisplayImages value={value} />;
+          })}
         </Wrapper>
       </Wrapper>
     </Wrapper>
