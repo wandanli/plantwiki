5f14f32ce14e0ae8abd0702d89214418ae6e0be0
danica <liwandan@gmail.com>
2021-03-30 23:05:10 +1300

    Displayed selected key images

--- src/components/detail-page/ImagePart.js
+++ src/components/detail-page/ImagePart.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import styled from "styled-components";
-import { Heading, Image, Paragraph, Wrapper } from "../../theme/globalStyle";
+import {
+  Heading,
+  Image,
+  Paragraph,
+  Wrapper,
+  Button,
+} from "../../theme/globalStyle";
 
 const StyledImage = styled(Image)`
   margin: 20px;
@@ -8,27 +14,76 @@ const StyledImage = styled(Image)`
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 `;
 
+const StyledWrapper = styled(Wrapper)`
+  /* display: ${(props) => (props.className === "" ? "block" : "none")}; */
+`;
+
 const ImagePart = ({ images }) => {
+  const [imagesKey, setImagesKey] = useState([]);
+
+  const getImagesKey = () => {
+    const imagesKeyArray = [];
+    for (let key in images) {
+      imagesKeyArray.push(key);
+    }
+    setImagesKey(imagesKeyArray);
+  };
+
+  useEffect(() => {
+    getImagesKey();
+  }, [images]);
+
+  const DisplayImages = ({ value }) => {
+    return (
+      <Wrapper flexDirection="column">
+        <StyledWrapper>
+          {value.map((el) => (
+            <StyledImage
+              key={el.id}
+              src={el.image_url}
+              width="300"
+              height="300"
+            ></StyledImage>
+          ))}
+        </StyledWrapper>
+      </Wrapper>
+    );
+  };
+
   return (
-    <Paragraph>
-      {Object.entries(images).map(([key, value]) => (
-        <Wrapper id={key} flexDirection="column">
-          <Heading h2 margin="20px">
-            Images {key === "" ? "" : `: ${key}`}
-          </Heading>
-          <Wrapper>
-            {value.map((el) => (
-              <StyledImage
-                key={el.id}
-                src={el.image_url}
-                width="300"
-                height="300"
-              ></StyledImage>
-            ))}
-          </Wrapper>
+    <Wrapper>
+      <Wrapper flexDirection="column">
+        <Heading h2 margin="20px">
+          Images
+        </Heading>
+        <Wrapper>
+          {Object.entries(images).map(([key, value]) => {
+            if (key === "") {
+              return (
+                <Wrapper flexDirection="column">
+                  <StyledWrapper>
+                    {value.map((el) => (
+                      <StyledImage
+                        key={el.id}
+                        src={el.image_url}
+                        width="300"
+                        height="300"
+                      ></StyledImage>
+                    ))}
+                  </StyledWrapper>
+                </Wrapper>
+              );
+            }
+            return null;
+          })}
         </Wrapper>
-      ))}
-    </Paragraph>
+        <Wrapper>
+          {imagesKey.map((key, index) => (
+            <Button key={index}>{key}</Button>
+          ))}
+        </Wrapper>
+      </Wrapper>
+    </Wrapper>
   );
 };
 
