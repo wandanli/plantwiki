9fdbd37e08c8cd2683630cc4b0e9873ca8add0ad
danica <liwandan@gmail.com>
2021-04-20 12:12:05 +1200

    Added github link background color

--- src/components/GithubLink.js
+++ src/components/GithubLink.js
@@ -5,22 +5,31 @@ import { MaxWidthBreakpoints } from "../theme/globalStyle";
 
 const StyledLink = styled.a`
   position: absolute;
-  top: 20px;
-  right: 20px;
-  font-size: 4rem;
-  color: ${(props) => props.theme.color.black};
-  @media ${MaxWidthBreakpoints.small} {
-    top: 10px;
-    right: 10px;
+  top: 10px;
+  right: 10px;
+  font-size: 3rem;
+  color: ${(props) => props.theme.color.greyDark};
+  /* @media ${MaxWidthBreakpoints.small} {
     font-size: 2.6rem;
-  } ;
+  } ; */
+`;
+
+const StyledDiv = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 50px;
+  background-color: ${(props) => props.theme.color.greyLight};
 `;
 
 const GithubLink = () => {
   return (
-    <StyledLink target="_blank" href="https://github.com/wandanli/plantwiki">
-      <FaGithub />
-    </StyledLink>
+    <StyledDiv>
+      <StyledLink target="_blank" href="https://github.com/wandanli/plantwiki">
+        <FaGithub />
+      </StyledLink>
+    </StyledDiv>
   );
 };
 
